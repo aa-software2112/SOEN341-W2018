e24a25f4a9f377492524469155ffd4a0ee31e03c
Alexander Chung <achung95@hotmail.com>
2018-02-16 12:32:16 -0500

    Latest version of app.js
    latest version of app.js as of feb 16th

--- FrontEnd/Website/app.js
+++ FrontEnd/Website/app.js
@@ -1,21 +1,6 @@
 // Set up express, body parser, and ejs
 var express = require("express");
-
-// Database 
-const mysql = require('mysql');
-var db = require('./database/database');
-
-
 var app = express();
-
-// Routes 
-// var router = express.Router();
-// router.get("/",function(req,res){
-// 	res.json({"message" : "Hello World"});
-// });
-
-// app.use("/api",router);
-
 var util = require('util');
 var bodyParser = require("body-parser");
 var date = require('date-and-time');
@@ -28,15 +13,25 @@ app.use( express.static( "public/about_team_img" ) );
 // Links express to the stylesheets
 app.use(express.static(__dirname + "/public"));
 
-/** <<<<<<<<<<<<<<< DATABASE CONNECTION >>>>>>>>>>>>>>
-* ============================================================================
-* 
-* ============================================================================
-*/
+var qId = "12";
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "soen341_project"
+});
 
 
-
-
+con.connect(function(err){
+	if (err){
+		throw err;
+	}
+	console.log(' connected ! :D');
+});
+  
 /* INSERTS PAGE GET/POST CONNECTIONS HERE */
 
 /** <<<<<<<<<<<<<<< Homepage >>>>>>>>>>>>>>
@@ -67,14 +62,14 @@ app.get(['/', '/home'], (req, res) => {
 	ORDER BY num_votes DESC \
 	LIMIT 10;"
 	
-	db.query(sql, function (err, result) {
+	con.query(sql, function (err, result) {
 		if (err) {
 			res.status(500).json({"status_code": 500,"status_message": "internal server error"});
 		} else {
 			
 			console.log(result);
 			
-			const output = {
+		const output = {
 				/*
 				* ---------------------------------------------------------------------------
 				* > Object newest
@@ -149,17 +144,8 @@ app.get(['/', '/home'], (req, res) => {
 			res.render('homepage.ejs', {homepage: output});
 		};	
 		// Populate the homepage.ejs file with the output object.
-		
-		
-		
 	});
-	
-	
-	
-});
-
-
-
+});	
 
 /* Listens for the question page request - done through the search */
 app.get("/question_forum/:q_id", function(req, res) {
@@ -170,164 +156,275 @@ app.get("/question_forum/:q_id", function(req, res) {
 		res.render('invalid_page', null);
 		return;
 	}
-	
+	//query from question table joined with user's table user_id
+	var sql = "select question.question_title,question.question_body, question.datetime_asked, question.question_id, user.username AS asked_by FROM question JOIN user ON question.user_id = user.user_id WHERE question_id = ?";
+	con.query(sql,[qId], function(err,result){
+  if(err){
+    console.log(err);
+    return;
+  }
+      //debugging to show question object
+      console.log(result);
+
+       //second query getting answer table and also joined with user's table user_id (yes, this is a query inside a query)
+      var sql2 ="select answer.answer_body, answer.answer_id, answer.datetime_answered, user.username AS answered_by FROM answer JOIN user ON answer.user_id = user.user_id WHERE answer.question_id = ?";
+      con.query(sql2,[qId], function(err,answer){
+
+      	//debugging
+      	console.log(answer);
+
+      	//Object to be sent to forum page
+		var outputQ = {
+
+      	//takes the last object in the result array and gets its respective parameter 
+      	q_id : result[result.length-1].question_id,
+      	title : result[result.length-1].question_title,
+      	body : result[result.length-1].question_body,
+
+      	// following code implemented using SQL2 query
+      	user_asked: result[result.length-1].asked_by,
+		question_pts: 10,
+		datetime_asked: result[result.length-1].datetime_asked,
+
+		// for answers, we store all the answers related to the given question_id in an array, where we iterate through the ANSWER object that was posted to the database from SQL2 query
+		answers:      
+			(function() {
+				arr = [];
+				var num_answers = answer.length;
+				for (var i = 0; i<num_answers; i++)
+					arr.push(
+					{
+						answer: answer[i].answer_body,
+						user_answered: answer[i].answered_by,
+						answer_pts: Math.round(Math.random()*1000 + 1),
+						datetime_answered: answer[i].datetime_answered
+					});
+		
+				return arr
+			})()
+      };
+     //not using these for now
 	// Get the entire page's data --> var outputObj = get_forum(question_id);
-	var outputObj = {
+	/*var outputObj = {
 		q_id: req.params.q_id,
-		title: "What is polymorphism in Java, and how can I test it?",
-		body: "I often hear my professor lecture about polymorphism, but I think I missed the class where he " +
-		"explained what it was (the snooze button :/ ). Can someone refresh me on the ins-and-outs of polymorphism, " +
-		"specifically, its implementation within java",
+		title: "What is polymorphism",
+		body: "I often hear",
 		user_asked: "Anthony2112",
 		question_pts: 10,
 		date_asked: date.format(new Date(), 'DD/MM/YYYY'),
 		time_asked: date.format(new Date(), 'h:m A').toUpperCase(),
 		answers:      
-		(function() {
-			arr = [];
-			var num_answers = Math.round(Math.random()*15)+1;
-			for (var i = 0; i<num_answers; i++)
-			arr.push(
-				{
-					answer: "From what I remember in my COMP 249 class @ Concordia University," +
-					"polymorphism is when an object takes on different forms based on the left-hand-side" +
-					"type and the right-hand-side object.",
-					user_answered: (function() {
-						var size = Math.round(Math.random()*10) + 1; // Size of username
-						var uname = new String("");
-						
-						for(var s = 0; s<size; s++)
-						uname = uname.concat(
-							String.fromCharCode(Math.round(Math.random()*42) + 48)
-						);
-						
-						return uname;
-					})(),
-					answer_pts: Math.round(Math.random()*1000 + 1),
-					date_answered: date.format(new Date(), 'DD/MM/YYYY'),
-					time_answered: date.format(new Date(), 'h:m A').toUpperCase()
-				});
-				
+			(function() {
+				arr = [];
+				var num_answers = Math.round(Math.random()*15)+1;
+				for (var i = 0; i<num_answers; i++)
+					arr.push(
+					{
+						answer: "From what I remember in my COMP 249 class @ Concordia University," +
+						"polymorphism is when an object takes on different forms based on the left-hand-side" +
+						"type and the right-hand-side object.",
+						user_answered: (function() {
+							var size = Math.round(Math.random()*10) + 1; // Size of username
+							var uname = new String("");
+							
+							for(var s = 0; s<size; s++)
+								uname = uname.concat(
+								String.fromCharCode(Math.round(Math.random()*42) + 48)
+								);
+							
+							return uname;
+						})(),
+						answer_pts: Math.round(Math.random()*1000 + 1),
+						date_answered: date.format(new Date(), 'DD/MM/YYYY'),
+						time_answered: date.format(new Date(), 'h:m A').toUpperCase()
+					});
+		
 				return arr
 			})()
-		};
-		
-		
-		
-		console.log("Requested Question Forum q_id = " + util.inspect(req.params));
-		// if q_id is valid
-		res.render('forum_page.ejs', {forum: outputObj});
-		// else
-		// res.render('invalid_page.ejs', null);
-		
+	};*/
+	
+	
+	
+	//console.log("Requested Question Forum q_id = " + outputQ.q_id);
+	// if q_id is valid
+	res.render('forum_page.ejs', {forum: outputQ});
+	// else
+	// res.render('invalid_page.ejs', null);
+				/* Listens for an answer from the forum page */
+
+//POST THE ANSWER ON THE ANSWER BOX, THERE IS A QUERRY INSIDE. AND YES THIS APP.POST IS INSIDE THE APP.GET FROM ABOVE
+app.post("/answer_to/:q_id", function(req, res) {
+	//OBJECTED TO BE POSTED TO ANSWER TABLE
+	var newA ={
+		answer_body : req.body.answer_body,
+		user_id : "1", //This can be changed to any user id that is in our local table, to be modified for log in implementation
+		question_id : outputQ.q_id,
+		datetime_answered :  date.format(new Date(), 'YYYY-MM-DD h:m:s'), 
+	}
+	
+	//MYSQL QUERRY
+	 var sql =" insert into answer set ?";
+con.query(sql, newA, function(err,result){
+ 	if(err){
+ 		console.log(err);
+ 		return;
+ 	}
+ 	console.log("Answer succesfully added ")
+     res.redirect("/question_forum/"+req.params.q_id);
+});
+});
+      });
+      
+	
+});
+
 	});
+
+/* Listens for an answer from the forum page 
+app.post("/answer_to/:q_id", function(req, res) {
+	var newA ={
+		answer_body : req.body.answer_body,
+		user_id : "2",
+		question_id : ,
+		datetime_answered :  date.format(new Date(), 'YYYY-MM-DD h:m:s'), 
+	}
 	
-	/* Listens for an answer from the forum page */
-	app.post("/answer_to/:q_id", function(req, res) {
-		
-		console.log("Received Answer!");
-		console.log(util.inspect(req.body) + " q_id : " + req.params.q_id);
-		res.redirect("/question_forum/"+req.params.q_id);	
-	});
-	
-	/* listens for a request from about us page and loads it*/
-	app.get("/about", function(req,res){
-		res.render('about.ejs');
-	});
-	
-	/* Listens to conctact us page request and loads it*/
-	app.get("/contact" , function(req,res){
-		res.render('contact.ejs');
-	});
-	
-	// Listens for a request to the search page and renders it with the result21s
-	app.get("/search", function(req, res) {
-		// Titles will be passed in once hooked up to DB
-		res.render('search_page.ejs');
-	});
-	
-	// Gets data from the search bar
-	app.post("/search", function(req, res) {
-		var search = req.body.search;
-		console.log(req.body);
-		res.redirect("/search");
-	});
-	
-	//listens for signup page request to load it
-	app.get("/sign_up", function(req,res){
-		res.render('sign_up.ejs');
-	});
-	
-	//listens for login page request to load it
-	app.get("/login", function(req,res){
-		res.render('login_page.ejs');
-	});
-	
-	/* Listens for user input from conctact us page*/
-	app.post("/contact", function(req,res){
-		var fname = req.body.firstname;
-		var lname = req.body.lastname;
-		var country = req.body.country;
-		//console.log (" Thank you "+ fname + " "+ lname + " from "+ country + " for contacting us.");
-		console.log(req.body);
-		res.redirect("/contact");
-	});
-	
-	// logs info from user from signup page
-	app.post("/sign_up", function(req,res){
-		var fName = req.body.fName;
-		var lName = req.body.lName;
-		var uName = req.body.uName;
-		var email = req.body.email;
-		var password = req.body.password;
-		var country = req.body.country;
-		var month = req.body.month;
-		var day = req.body.day;
-		var year = req.body.year;
-		var gender = req.body.gender;
-		
-		//console.log("New user added:");    
-		//console.log ("First Name: " + fName + "\nLast Name: " + lName + "\nUsername: " + uName + "\nEmail: " + email + "\nPassword: " + "\nCountry: " + country + 
-		//	"\nDate of Birth: " + month+"/" + day + "/" + year + "\nGender: " + gender);
-		console.log(req.body);
-		res.redirect("/sign_up");
-	});
-	
-	// logs info from user from login page
-	app.post("/login", function(req,res){
-		var email = req.body.email;
-		var password = req.body.password;
-		console.log(req.body);
-		res.redirect("/login");
-	});
-	
-	app.get("/ask", function(req, res) {
-		res.render("ask.ejs");
-	});
-	
-	//logs of questions from the ask_questions
-	app.post("/askform" , function(req,res){
-		var q_title = req.body.q_title;
-		var q_body = req.body.q_body;
-		console.log(req.body);
-		res.redirect("/question_forum/234");
-	});
-	
-	app.get("/user_profile", function (req, res) {
-		res.render("user_profile.ejs");
-	});
-	
-	
-	
-	// Catches invalid URL requests
-	app.get("*", function(req, res) {
-		res.render('invalid_page', null);
-	}) ;
-	
-	// Makes the server listen for req/res - http://localhost:3000
-	app.listen(3000, function() {
-		console.log("Server Running on Port 3000");
-		console.log("Working Directory: " + __dirname);
-	});
-	
-	
+	 var sql =" insert into answer set ?";
+con.query(sql, newA, function(err,result){
+ 	if(err){
+ 		console.log(err);
+ 		return;
+ 	}
+ 	console.log("Answer succesfully added ")
+     res.redirect("/question_forum/"+req.params.q_id);
+});
+});*/
+
+/* listens for a request from about us page and loads it*/
+app.get("/about", function(req,res){
+	res.render('about.ejs');
+});
+
+/* Listens to conctact us page request and loads it*/
+app.get("/contact" , function(req,res){
+     res.render('contact.ejs');
+});
+
+// Listens for a request to the search page and renders it with the results
+app.get("/search", function(req, res) {
+	// Titles will be passed in once hooked up to DB
+	res.render('search_page.ejs');
+});
+
+// Gets data from the search bar
+app.post("/search", function(req, res) {
+	var search = req.body.search;
+	console.log(req.body);
+	res.redirect("/search");
+});
+
+//listens for signup page request to load it
+app.get("/sign_up", function(req,res){
+	res.render('sign_up.ejs');
+});
+
+//listens for login page request to load it
+app.get("/login", function(req,res){
+	res.render('login_page.ejs');
+});
+
+/* Listens for user input from conctact us page*/
+app.post("/contact", function(req,res){
+     var fname = req.body.firstname;
+     var lname = req.body.lastname;
+     var country = req.body.country;
+     //console.log (" Thank you "+ fname + " "+ lname + " from "+ country + " for contacting us.");
+     console.log(req.body);
+     res.redirect("/contact");
+});
+
+// logs info from user from signup page
+app.post("/sign_up", function(req,res){
+
+	//THIS IS THE OBJECT TO BE POSTED IN THE DATABSE USER TABLE. EVERY DATA IS GOTTEN FROM THE SIGN UP FORM
+      var newUser = {
+
+ 	first_name : req.body.fName,
+ 	last_name : req.body.lName,
+ 	username : req.body.uName,
+ 	email : req.body.email,
+ 	password : req.body.password,
+ 	country : req.body.country,
+ 	birth_date : req.body.year +"-" + req.body.month + "-"+req.body.day,
+ 	gender : req.body.gender,
+ 	datetime_entered : date.format(new Date(), 'YYYY-MM-DD h:m:s'), 
+
+ };
+
+//MYSQL QUERRY (MIGHT NEED MODIFICATIONS)
+ var sql =" insert into user set ?";
+con.query(sql, newUser, function(err,result){
+ 	if(err){
+ 		console.log(err);
+ 		return;
+ 	}
+ 	console.log("User succesfully added ")
+     res.redirect("/sign_up");
+});
+});
+
+// logs info from user from login page
+app.post("/login", function(req,res){
+     var email = req.body.email;
+     var password = req.body.password;
+	 console.log(req.body);
+     res.redirect("/login");
+});
+
+app.get("/ask", function(req, res) {
+	res.render("ask.ejs");
+});
+
+//logs of questions from the ask_questions
+app.post("/askform" , function(req,res){
+	qId++;
+	var newQ = {
+		question_title : req.body.q_title,
+		question_body: req.body.q_body,
+		user_id: "1",
+		datetime_asked : date.format(new Date(), 'YYYY-MM-DD h:m:s'),
+
+};
+var sql2 =" insert into question set ?";
+
+con.query(sql2, newQ, function(err,result){
+ 	if(err){
+ 		console.log(err);
+ 		return;
+ 	}
+ 	console.log("Question possted ");
+ 	console.log(newQ.question_id);
+ 	con.query("select * from question", function(err,qId){
+ 		var qId = qId[qId.length-1].question_id;
+ 		res.redirect("/question_forum/"+qId);
+ 	})
+     
+});
+});
+
+app.get("/user_profile", function (req, res) {
+	res.render("user_profile.ejs");
+});
+
+
+
+// Catches invalid URL requests
+app.get("*", function(req, res) {
+	res.render('invalid_page', null);
+}) ;
+
+// Makes the server listen for req/res - http://localhost:3000
+app.listen(3000, function() {
+	console.log("Server Running on Port 3000");
+	console.log("Working Directory: " + __dirname);
+});
