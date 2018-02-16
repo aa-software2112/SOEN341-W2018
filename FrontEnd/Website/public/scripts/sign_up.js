3e30d95816f33e5de45dafc93eefa536d941bea2
Abishand <35385222+AA-97@users.noreply.github.com>
2018-02-16 00:27:12 -0500

    added js file for signup page

---
+++ FrontEnd/Website/public/scripts/sign_up.js
@@ -0,0 +1,37 @@
+function checkLogin(){
+				return checkPassword();
+			}
+
+
+
+			function checkPassword(){
+				var dom = document.getElementById("password");
+				var password = dom.value;
+				var letter = password.search(/[A-Z]/);
+				var number = password.search(/[0-9]/);
+				var letterCheck;
+				var numberCheck;
+				//checks if it contains at least one number and letter
+				letterCheck = (letter != -1);
+				numberCheck = (number != -1);
+
+				//checks if pass contains letters and numbers only
+				var ok = password.search(/^[A-Za-z0-9]+$/);
+
+				var returnVal;
+
+				//checks if password is at least 4 characters
+				if((ok == 0) && (password.length >= 4))
+					returnVal = (letterCheck && numberCheck);
+				else
+					returnVal = false;
+
+				if(returnVal)
+					return true;
+				else{
+					alert("Username or password does not match the allowed format");
+					return false;
+				}
+
+
+			}
