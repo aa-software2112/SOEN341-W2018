30962e4c9ae180c1e27a33272af45f64c45ea601
Alexander Chung <achung95@hotmail.com>
2018-02-16 23:05:06 -0500

    Updates main app.js file
    Added a query to database for usage on homepage get function.
    
    Code implementation to show the newest questions posted that are being
    pulled from the database from line 74 to 146. Homepage dynamically
    updates the question every time a new question is posted

--- FrontEnd/Website/app.js
+++ FrontEnd/Website/app.js
@@ -44,101 +44,110 @@ con.connect(function(err){
 * ============================================================================
 */
 
-app.get(
-	['/', '/home_page.html'],
-	function(req, res) {
-		
+app.get(['/', '/home'], (req, res) => {
+	
+	
+	var sql = "SELECT question.question_title, user.username AS asked_by, answer.answer_body AS answers, \
+	(SELECT COUNT(*) FROM score_question WHERE question_id=question.question_id) AS num_votes, \
+	(SELECT COUNT(*) FROM answer WHERE question_id=question.question_id) AS num_views, \
+	datetime_asked \
+	FROM question JOIN user ON question.user_id=user.user_id JOIN answer ON question.user_id=answer.user_id \
+	WHERE (answer.question_id is NOT NULL) OR (answer.question_id IS NULL) \
+	ORDER BY datetime_asked DESC \
+	LIMIT 10;";
+	
+	var query_popular = "SELECT question.question_title, user.username AS asked_by, answer.answer_body AS answers, \
+	(SELECT COUNT(*) FROM score_question WHERE question_id=question.question_id) AS num_votes, \
+	(SELECT COUNT(*) FROM answer WHERE question_id=question.question_id) AS num_views, \
+	datetime_asked \
+	FROM question JOIN user ON question.user_id=user.user_id JOIN answer ON question.user_id=answer.user_id \
+	ORDER BY num_votes DESC \
+	LIMIT 10;"
+	
+	con.query(sql, function (err, result) {
+		if (err) {
+			res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+		} else {
+			
+			console.log(result);
+			
 		const output = {
-			/*
-			* ---------------------------------------------------------------------------
-			* > Object newest
-			* username, question, # of votes, # of answers, # of views, date and time
-			* ---------------------------------------------------------------------------
-			*/
-			newest: {
-				newest_question_list:
-				(function() {
-					arr = [];
-					var num_of_questions = 10;
-					for (var i = 0; i < num_of_questions; i++)
-					arr.push(
-						{
-							userName: (function() {
-								var text = "";
-								var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-								
-								for (var i = 0; i < 5; i++)
-								text += possible.charAt(Math.floor(Math.random() * possible.length));
-								
-								return text;
-							})(),
-							question: (function() {
-								var text = "";
-								var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-								
-								for (var i = 0; i < 200; i++)
-								text += possible.charAt(Math.floor(Math.random() * possible.length));
-								
-								return text;
-							})(),
-							numOfVotes: Math.floor(Math.random() * 100), 
-							numOfAnswers: Math.floor(Math.random() * 100),
-							numOfViews: Math.floor(Math.random() * 100),
-							date_ans: date.format(new Date(), 'DD/MM/YYYY'),
-							time_ans: date.format(new Date(), 'h:m A').toUpperCase()
-						}
-					);
-					return arr
-				})()
-			},
-			
-			/** 
-			* ---------------------------------------------------------------------------
-			* > Object newest
-			* ---------------------------------------------------------------------------
-			*/
-			popular: {
-				popular_question_list:
-				(function() {
-					arr = [];
-					var num_of_questions = 10;
-					for (var i = 0; i < num_of_questions; i++)
-					arr.push(
-						{
-							userName: (function() {
-								var text = "";
-								var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-								
-								for (var i = 0; i < 5; i++)
-								text += possible.charAt(Math.floor(Math.random() * possible.length));
-								
-								return text;
-							})(),
-							question: (function() {
-								var text = "";
-								var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-								
-								for (var i = 0; i < 200; i++)
-								text += possible.charAt(Math.floor(Math.random() * possible.length));
-								
-								return text;
-							})(),
-							numOfVotes: Math.floor(Math.random() * 100), 
-							numOfAnswers: Math.floor(Math.random() * 100),
-							numOfViews: Math.floor(Math.random() * 100),
-							date_ans: date.format(new Date(), 'DD/MM/YYYY'),
-							time_ans: date.format(new Date(), 'h:m A').toUpperCase()
-						}
-					);
-					arr.sort(sortBy('-numOfVotes'));
-					return arr
-				})()
+				/*
+				* ---------------------------------------------------------------------------
+				* > Object newest
+				* username, question, # of votes, # of answers, # of views, date and time
+				* ---------------------------------------------------------------------------
+				*/
+				
+				newest: {
+					newest_question_list:
+					(function() {
+						var newestQuestionList = [];
+						var num_of_questions = 10;
+						
+						for (var i = 0; i < num_of_questions; i++) {
+							var questions = {
+								userName: result[i].asked_by,
+								question: result[i].question_title,
+								numOfVotes: result[i].num_votes,
+								numOfAnswers: result[i].num_views,
+								date_ans: result[i].datetime_asked							
+							}
+							newestQuestionList.push(questions);
+						}						
+						return newestQuestionList
+					})()
+				},
+				
+				/** 
+				* ---------------------------------------------------------------------------
+				* > Object newest
+				* ---------------------------------------------------------------------------
+				*/
+				popular: {
+					popular_question_list:
+					(function() {
+						popularQuestionList = [];
+						var num_of_questions = 10;
+						for (var i = 0; i < num_of_questions; i++)
+						popularQuestionList.push(
+							{
+								userName: (function() {
+									var text = "";
+									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+									
+									for (var i = 0; i < 5; i++)
+									text += possible.charAt(Math.floor(Math.random() * possible.length));
+									
+									return text;
+								})(),
+								question: (function() {
+									var text = "";
+									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+									
+									for (var i = 0; i < 200; i++)
+									text += possible.charAt(Math.floor(Math.random() * possible.length));
+									
+									return text;
+								})(),
+								numOfVotes: Math.floor(Math.random() * 100), 
+								numOfAnswers: Math.floor(Math.random() * 100),
+								numOfViews: Math.floor(Math.random() * 100),
+								date_ans: date.format(new Date(), 'DD/MM/YYYY'),
+								time_ans: date.format(new Date(), 'h:m A').toUpperCase()
+							}
+						);
+						popularQuestionList.sort(sortBy('-numOfVotes'));
+						return popularQuestionList
+					})()
+				}
+				
 			}
-			
+			res.render('homepage.ejs', {homepage: output});
 		};	
 		// Populate the homepage.ejs file with the output object.
-		res.render('homepage.ejs', {homepage: output});
 	});
+});	
 
 /* Listens for the question page request - done through the search */
 app.get("/question_forum/:q_id", function(req, res) {
