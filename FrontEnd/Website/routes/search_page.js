876149bef86b97fcea8c9881d51c731965a72a53
unknown <serranotom@gmail.com>
2018-02-28 17:44:06 -0500

    Passed the search string to the search page so that it display on top of all the results. JS and EJS modified.

--- FrontEnd/Website/routes/search_page.js
+++ FrontEnd/Website/routes/search_page.js
@@ -18,16 +18,17 @@ var db = require('../database/database');
 */
 
 // Listens for a request to the search page and renders it with the results
+// GET command should not be used - user should only get to search page via a search POST
 router.get('/', (req, res) => {
 	// Titles will be passed in once hooked up to DB
 	res.render('search_page.ejs');
 });
 
-// Gets data from the search bar
+// Gets data from the search bar and renders the search page with the results
 router.post('/', (req, res) => {
 	var search = req.body.search;
 	console.log(req.body);
-	res.redirect("/search");
+	res.render('search_page.ejs', {item: search});
 });
 
 module.exports = router;
