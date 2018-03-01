68c93bced93f4743f955b54b749c515f1f91497a
Jason <jasonckc07@gmail.com>
2018-03-01 17:03:10 -0500

    Add the homepage tab functionality and redirecting each question link to the question_forum page
    - The "Newest" and "Popular" tab worked as intended. The homepage.ejs was separated into two files: homepage_newest.ejs and homepage_popular.ejs to allow the functionality. The route hasn't been modified and the tab uses a value from the 'href' attribute in a link tag.
    
    - Modified the queries for the top 10 newest and popular questions in the SQL_QUERIES as it allows duplicates.
    
    - Clicking on a question title on the homepage should redirect the user to the right question page using the right question_id in the GET method.

--- FrontEnd/Website/routes/homepage.js
+++ FrontEnd/Website/routes/homepage.js
@@ -6,6 +6,7 @@ var util = require('util');
 var bodyParser = require("body-parser");
 var date = require('date-and-time');
 var sortBy = require('sort-by');
+var url = require('url');
 
 // Database connection
 const mysql = require('mysql');
@@ -22,186 +23,168 @@ var db = require('../database/database');
 
 router.get(['/', '/home'], (req, res) => {
 	
-	console.log("in home!");
-	
-	var sql = "SELECT question.question_id, question.question_title, user.username AS asked_by, answer.answer_body AS answers, \
-	(SELECT COUNT(*) FROM score_question WHERE question_id=question.question_id) AS num_votes, \
-	(SELECT COUNT(*) FROM answer WHERE question_id=question.question_id) AS num_views, \
-	datetime_asked \
-	FROM question JOIN user ON question.user_id=user.user_id JOIN answer ON question.user_id=answer.user_id \
-	WHERE (answer.question_id is NOT NULL) OR (answer.question_id IS NULL) \
-	ORDER BY datetime_asked DESC \
-	LIMIT 10;";
-	
-	var query_popular = "SELECT question.question_title, user.username AS asked_by, answer.answer_body AS answers, \
-	(SELECT COUNT(*) FROM score_question WHERE question_id=question.question_id) AS num_votes, \
-	(SELECT COUNT(*) FROM answer WHERE question_id=question.question_id) AS num_views, \
-	datetime_asked \
-	FROM question JOIN user ON question.user_id=user.user_id JOIN answer ON question.user_id=answer.user_id \
-	ORDER BY num_votes DESC \
-	LIMIT 10;";
-	
-	db.query(sql, function (err, result) {
-		if (err) {
-			res.status(500).json({"status_code": 500,"status_message": "internal server error"});
-		} else {
-			
-			console.log(result);
-			
-		var output;
-			
-		if (result.length == 0)
-		{
-			
-			output = {
-				newest: {
-					newest_question_list:
-					(function() {
-						var newestQuestionList = [];
-						var num_of_questions = 10;
+	if(!req.query.tab || req.query.tab === 'newest') { 
+		
+		var query_newest = "SELECT question.question_id, question.question_title, user.username AS asked_by, \
+		(SELECT COUNT(*) FROM score_question WHERE question_id=question.question_id) AS num_votes, \
+		(SELECT COUNT(*) FROM answer WHERE question_id=question.question_id) AS num_views, \
+		datetime_asked \
+		FROM question JOIN user ON question.user_id=user.user_id \
+		ORDER BY datetime_asked DESC \
+		LIMIT 10;";
+		
+		db.query(query_newest, function (err, result) {
+			if (err) {
+				res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+			} else {
+				var output;
+				if (result.length == 0) 
+				{
+					output = {
+						newest: {
+							newest_question_list:
+							(function() {
+								var newestQuestionList = [];
+								var num_of_questions = 10;
+								
+								for (var i = 0; i < num_of_questions; i++) {
+									
+									var questions = {
+										userName: ' ',
+										questionTitle: ' ',
+										numOfVotes: ' ',
+										numOfAnswers: ' ',
+										date_ans: ' '						
+									}
+									newestQuestionList.push(questions);
+								}						
+								return newestQuestionList
+							})()
+						}
+					}
+				} else {
+					output = {
 						
-						for (var i = 0; i < num_of_questions; i++) {
-							var questions = {
-								userName: "test",
-								question: "test question",
-								numOfVotes: 1,
-								numOfAnswers: 1,
-								date_ans: "2018-25-02 13:14:15"						
-							}
-							newestQuestionList.push(questions);
-						}						
-						return newestQuestionList
-					})()
+						/*
+						* ---------------------------------------------------------------------------
+						* > Object newest
+						* username, question, # of votes, # of answers, # of views, date and time
+						* ---------------------------------------------------------------------------
+						*/
+						
+						newest: {
+							newest_question_list:
+							(function() {
+								var newestQuestionList = [];
+								var num_of_questions = 10;
+								
+								for (var i = 0; i < num_of_questions; i++) {
+									
+									var d = (new Date(result[i].datetime_asked)).toISOString().split("T");
+									d = d[0] + " - " + d[1].split("Z")[0].slice(0, -4);
+									
+									var questions = {
+										questionID: result[i].question_id,
+										userName: result[i].asked_by,
+										questionTitle: result[i].question_title,
+										numOfVotes: result[i].num_votes,
+										numOfAnswers: result[i].num_views,
+										date_ans: d				
+									}
+									newestQuestionList.push(questions);
+								}						
+								return newestQuestionList
+							})()
+						}									
+					}
+				}
+				res.render('homepage_newest.ejs', {homepage: output});
+			};	
+		});
+		
+	} else if (req.query.tab === 'popular') {
+		var query_popular = "SELECT question.question_id, question.question_title, user.username AS asked_by,  \
+		(SELECT COUNT(*) FROM score_question WHERE question_id=question.question_id) AS num_votes, \
+		(SELECT COUNT(*) FROM answer WHERE question_id=question.question_id) AS num_views, \
+		datetime_asked \
+		FROM question JOIN user ON question.user_id=user.user_id \
+		ORDER BY num_votes DESC \
+		LIMIT 10;";
+		
+		db.query(query_popular, function (err, result) {
+			if (err) {
+				res.status(500).json({"status_code": 500,"status_message": "internal server error"});
+			} else {
+				var output; 
+				
+				if (result.length == 0)
+				{
+					output = {
+						popular: {
+							popular_question_list:
+							(function() {
+								var popularQuestionList = [];
+								var num_of_questions = 10;
+								
+								for (var i = 0; i < num_of_questions; i++) {
+									var questions = {
+										userName: ' ',
+										questionTitle: ' ',
+										numOfVotes: ' ',
+										numOfAnswers: ' ',
+										date_ans: ' '								
+									}
+									popularQuestionList.push(questions);
+								}						
+								return popularQuestionList
+							})()
+						},
+					}
 					
-				},
-				popular: {
-					popular_question_list:
-					(function() {
-						popularQuestionList = [];
-						var num_of_questions = 10;
-						for (var i = 0; i < num_of_questions; i++)
-						popularQuestionList.push(
-							{
-								userName: (function() {
-									var text = "";
-									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-									
-									for (var i = 0; i < 5; i++)
-									text += possible.charAt(Math.floor(Math.random() * possible.length));
-									
-									return text;
-								})(),
-								question: (function() {
-									var text = "";
-									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-									
-									for (var i = 0; i < 200; i++)
-									text += possible.charAt(Math.floor(Math.random() * possible.length));
-									
-									return text;
-								})(),
-								numOfVotes: Math.floor(Math.random() * 100), 
-								numOfAnswers: Math.floor(Math.random() * 100),
-								numOfViews: Math.floor(Math.random() * 100),
-								date_ans: date.format(new Date(), 'DD/MM/YYYY'),
-								time_ans: date.format(new Date(), 'h:m A').toUpperCase()
-							}
-						);
-						popularQuestionList.sort(sortBy('-numOfVotes'));
-						return popularQuestionList
-					})()
-				}
-			}
-			
-		}
-		else
-		{
-			
-			output = {
-				/*
-				* ---------------------------------------------------------------------------
-				* > Object newest
-				* username, question, # of votes, # of answers, # of views, date and time
-				* ---------------------------------------------------------------------------
-				*/
-				
-				newest: {
-					newest_question_list:
-					(function() {
-						var newestQuestionList = [];
-						var num_of_questions = result.length;
+				} else {
+					
+					output = {
+						/*
+						* ---------------------------------------------------------------------------
+						* > Object popular
+						* username, question, # of votes, # of answers, # of views, date and time
+						* ---------------------------------------------------------------------------
+						*/
 						
-						for (var i = 0; i < num_of_questions; i++) {
-							
-							var d = (new Date(result[i].datetime_asked)).toISOString().split("T");
-							d = d[0] + " - " + d[1].split("Z")[0].slice(0, -4);
-							
-							var questions = {
-								userName: result[i].asked_by,
-								question: result[i].question_title,
-								numOfVotes: result[i].num_votes,
-								numOfAnswers: result[i].num_views,
-								q_id: result[i].question_id,
-								date_ans: d			
-							}
-							newestQuestionList.push(questions);
-						}						
-						return newestQuestionList
-					})()
-				},
-				
-				/** 
-				* ---------------------------------------------------------------------------
-				* > Object newest
-				* ---------------------------------------------------------------------------
-				*/
-				popular: {
-					popular_question_list:
-					(function() {
-						popularQuestionList = [];
-						var num_of_questions = 10;
-						for (var i = 0; i < num_of_questions; i++)
-						popularQuestionList.push(
-							{
-								userName: (function() {
-									var text = "";
-									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+						popular: {
+							popular_question_list:
+							(function() {
+								var popularQuestionList = [];
+								var num_of_questions = 10;
+								
+								for (var i = 0; i < num_of_questions; i++) {
 									
-									for (var i = 0; i < 5; i++)
-									text += possible.charAt(Math.floor(Math.random() * possible.length));
+									var d = (new Date(result[i].datetime_asked)).toISOString().split("T");
+									d = d[0] + " - " + d[1].split("Z")[0].slice(0, -4);
 									
-									return text;
-								})(),
-								question: (function() {
-									var text = "";
-									var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-									
-									for (var i = 0; i < 200; i++)
-									text += possible.charAt(Math.floor(Math.random() * possible.length));
-									
-									return text;
-								})(),
-								numOfVotes: Math.floor(Math.random() * 100), 
-								numOfAnswers: Math.floor(Math.random() * 100),
-								numOfViews: Math.floor(Math.random() * 100),
-								date_ans: date.format(new Date(), 'DD/MM/YYYY'),
-								time_ans: date.format(new Date(), 'h:m A').toUpperCase()
-							}
-						);
-						popularQuestionList.sort(sortBy('-numOfVotes'));
-						return popularQuestionList
-					})()
+									var questions = {
+										questionID: result[i].question_id,
+										userName: result[i].asked_by,
+										questionTitle: result[i].question_title,
+										numOfVotes: result[i].num_votes,
+										numOfAnswers: result[i].num_views,
+										date_ans: d							
+									}
+									popularQuestionList.push(questions);
+								}						
+								return popularQuestionList
+							})()
+						},
+						
+					}
 				}
-				
-			}
+				res.render('homepage_popular.ejs', {homepage: output});
+			};	
 			
-			
-		}
-			
-			res.render('homepage.ejs', {homepage: output});
-		};	
-		// Populate the homepage.ejs file with the output object.
-	});
+		});
+	}
+	
 });
 
 module.exports = router;
+
