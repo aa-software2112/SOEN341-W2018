41cb302567ff8fa9fdee88139ae4f15af9bd1f4c
unknown <serranotom@gmail.com>
2018-03-04 18:04:28 -0500

    Added the framework for the Ask page POST test. Will not pass unless logged in

--- FrontEnd/Website/test/ask.test.js
+++ FrontEnd/Website/test/ask.test.js
@@ -1,11 +1,17 @@
 var request = require('supertest');
 var app = require('../app');
 
-
-// Pings the ask page GET function to make sure it responds properly (200 response)
-describe("ask", function () {
+describe("Ask Page", function () {
+	// Pings the ask page GET function to make sure it responds properly (200 response)
 	it("GET response - Ask page", function(done) {
 		request(app).get("/ask")
 			.expect(200, done);
+	});
+
+	// Checks to ensure that the Ask form data is being sent across the server (302 response)
+	it("POST data - Ask page (Expected Fail)", function(done) {
+		request(app).post("/ask")
+			.send({q_title: "TestTitle", q_body: "TestBody"})
+			.expect(302, done);
 	});
 });
