2495dfa5f26622caacc74d4ea72e30e706039d15
unknown <serranotom@gmail.com>
2018-03-04 15:32:21 -0500

    Fixed typo in signup.test and changed the userprofile.test to check for a redirect rather than a render

--- FrontEnd/Website/test/userprofile.test.js
+++ FrontEnd/Website/test/userprofile.test.js
@@ -2,10 +2,10 @@ var request = require('supertest');
 var app = require('../app');
 
 
-// Pings the user profile page GET function to make sure it responds properly (200 response)
+// Pings the user profile page GET function to make sure it responds properly (302 redirect response)
 describe("user profile", function () {
 	it("GET response - User Profile page", function(done) {
 		request(app).get("/user_profile")
-			.expect(200, done);
+			.expect(302, done);
 	});
 });
