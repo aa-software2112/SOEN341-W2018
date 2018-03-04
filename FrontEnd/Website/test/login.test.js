bae136e90ce914c9c9f0dc800e4459a22231114a
unknown <serranotom@gmail.com>
2018-03-04 15:13:45 -0500

    Created the file and code for the login page GET response test

---
+++ FrontEnd/Website/test/login.test.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+var app = require('../app');
+
+
+// Pings the login page GET function to make sure it responds properly (200 response)
+describe("login", function () {
+	it("GET response - Login page", function(done) {
+		request(app).get("/login_page")
+			.expect(200, done);
+	});
+});
