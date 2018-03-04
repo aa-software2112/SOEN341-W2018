9dee2b0d7719c141b87d5f7e1c3920b85ec153d8
unknown <serranotom@gmail.com>
2018-03-04 15:37:31 -0500

    Removed uneeded invalid page test and commented that the search test will fail until POST test is complete

--- FrontEnd/Website/test/search.test.js
+++ FrontEnd/Website/test/search.test.js
@@ -3,8 +3,9 @@ var app = require('../app');
 
 
 // Pings the search page GET function to make sure it responds properly (200 response)
+// Test expected to fail at the moment since it can only be reached once a search is initiated
 describe("search", function () {
-	it("GET response - Search page", function(done) {
+	it("GET response - Search page (Expected Fail)", function(done) {
 		request(app).get("/search")
 			.expect(200, done);
 	});
