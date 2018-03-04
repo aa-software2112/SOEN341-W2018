303ca8c23fd5c1ac1791d1b9a7aac748c2d7b7b5
unknown <serranotom@gmail.com>
2018-03-04 15:15:22 -0500

    Created the code and file for the search page GET response test

---
+++ FrontEnd/Website/test/search.test.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+var app = require('../app');
+
+
+// Pings the search page GET function to make sure it responds properly (200 response)
+describe("search", function () {
+	it("GET response - Search page", function(done) {
+		request(app).get("/search_page")
+			.expect(200, done);
+	});
+});
