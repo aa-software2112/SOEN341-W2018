832096f02d50da86ae2711f2fe3fd59e6ad80f20
unknown <serranotom@gmail.com>
2018-03-08 22:47:09 -0500

    Sign Up page POST test created in signup.test.js

--- FrontEnd/Website/test/signup.test.js
+++ FrontEnd/Website/test/signup.test.js
@@ -2,10 +2,18 @@ var request = require('supertest');
 var app = require('../app');
 
 
-// Pings the sign up page GET function to make sure it responds properly (200 response)
+// Pings the sign up page GET function to make sure it responds properly
 describe("sign up", function () {
 	it("GET response - Sign Up page", function(done) {
 		request(app).get("/sign_up")
+			.expect(200, done);
+	});
+
+		// Checks to ensure that form data is being sent over the server
+	it("POST data - Sign Up page", function(done) {
+		request(app).post("/sign_up")
+			.send({fName: "First", lName: "last", uName: "Tester", email: "testing@test.ca", password: "Test12", country: "Canada", 
+					year: "1905", month: "10", day: "12", gender: "other" })
 			.expect(200, done);
 	});
 });
