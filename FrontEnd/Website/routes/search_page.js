a6f7fe4033faae39a17a15f89fd15490fb8fbedb
Jason <jasonckc07@gmail.com>
2018-03-16 16:45:08 -0400

    Added search functionality
    The user is able to search for a specific keyword in the search form in the navbar. Once the keyword has been submitted, the user is redirected to the search_page.ejs

--- FrontEnd/Website/routes/search_page.js
+++ FrontEnd/Website/routes/search_page.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var path = require('path');
 var util = require('util');
 var bodyParser = require("body-parser");
+var cors = require('cors');
+var date = require('date-and-time');
+var dateFormat = require('dateformat');
 
 // Database connection
 const mysql = require('mysql');
@@ -24,11 +27,54 @@ router.get('/', (req, res) => {
 	res.render('search_page.ejs');
 });
 
+
+
 // Gets data from the search bar and renders the search page with the results
-router.post('/', (req, res) => {
-	var search = req.body.search;
-	console.log(req.body);
-	res.render('search_page.ejs', {item: search});
+router.get('/search_on', (req,res) => {
+	var keyword = req.query.search;
+	
+	/**
+	*  Query to match the keyword to the question title of the question's table in the database. Also return the
+	question id for redirecting the user to the forum page upon clicking on a matched question. 
+	*/
+	var search_keyword = 'SELECT question.question_id, question.question_title, question.user_id, user.username, question.datetime_asked FROM question, user WHERE Match(question_title) Against("'+ keyword +'" IN NATURAL LANGUAGE MODE) AND question.user_id = user.user_id';
+	
+	db.query(search_keyword, function(err, rows, fields) {
+		if (err){
+			console.log(err);
+		}
+		console.log(rows);
+		
+		// Stores the result into the array data, which is returned when the function search_question_list is called
+		var output = {
+			search_question: {
+				search_question_list:
+				(function() {
+					var data=[];
+					var number_of_questions = rows.length;
+					for (var i = 0; i < number_of_questions; i++) {
+						
+						var timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
+						var d = (new Date(rows[i].datetime_asked  - timezoneOffset)).toISOString().split("T");
+						d = d[0] + " - " + d[1].split("Z")[0].slice(0, -4);
+						
+						var searchList = {
+							questionID: rows[i].question_id,
+							questionList: rows[i].question_title,
+							userName: rows[i].username,
+							date_ask: d
+							
+						}						
+						data.push(searchList);
+					}
+					return data;
+					
+				})()
+			}
+		}
+		
+		res.render('search_page.ejs', {search_page: output});
+	})
 });
 
 module.exports = router;
