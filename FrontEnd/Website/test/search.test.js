896ca1f9bab3b959d948ee687ad84e4d75512d77
unknown <serranotom@gmail.com>
2018-03-21 16:54:24 -0400

    Completed the GET test for the search page by inputting a URL of a test search

--- FrontEnd/Website/test/search.test.js
+++ FrontEnd/Website/test/search.test.js
@@ -5,8 +5,7 @@ var app = require('../app');
 // Pings the search page GET function to make sure it responds properly (200 response)
 describe("search", function () {
 	it("Search page functionality", function(done) {
-		request(app).post("/search")
-			.send({search: "test"})
+		request(app).get("/search/search_on/?search=test")
 			.expect(200, done);
 	});
 });
