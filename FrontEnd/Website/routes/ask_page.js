b72e9f4f59af07af8e06c0f58bb953b0589a0ce3
Alexander Chung <achung95@hotmail.com>
2018-03-21 22:47:14 -0400

    Some minor code refactoring #303  #304
    Code reindentation, deleting some unnecessary white spaces, added some
    missing semi colons, changed name of queries to better showcase what is
    the corresponding query doing.

--- FrontEnd/Website/routes/ask_page.js
+++ FrontEnd/Website/routes/ask_page.js
@@ -17,57 +17,53 @@ var db = require('../database/database');
 * ============================================================================
 */
 
-
 router.get('/', (req, res) => {
 	res.render("ask.ejs");
 });
 
 //logs of questions from the ask_questions
 router.post("/askform/:user_id" , (req,res) => {
-	
+
 	console.log("Asked a question with user_id " + req.params.user_id);
-	
+
 	var newQ = {
 		question_title : req.body.q_title,
 		question_body: req.body.q_body,
 		user_id: req.params.user_id,
 		datetime_asked : date.format(new Date(), 'YYYY-MM-DD h:m:s')
 	};
-	var sql2 =" insert into question set ?";
-	
-	db.query(sql2, newQ, function(err,result){
+	var sql_insertQuestion =" insert into question set ?";
+
+	db.query(sql_insertQuestion, newQ, function(err,result){
 		if(err){
 			console.log(err);
 			return;
 		}
-
 		console.log("Question posted ");
-        
-        //query to get all the question ids of all the question from the user_id that is asking the question
-		var sql3 ="select question.question_id from question JOIN user ON question.user_id = user.user_id \
-		WHERE user.user_id = ?";
-		
-		//this is the user Id of the user askinng the question, that is taken out from the object newQ that was inserted in to the database
-		var userId= newQ.user_id;
 
-		db.query(sql3,[req.params.user_id], function(err,qId){
+    //query to get all the question ids of all the question from the user_id that is asking the question
+    var sql_selectQuestion ="select question.question_id from question JOIN user ON question.user_id = user.user_id \
+    WHERE user.user_id = ?";
 
-			//debugging purpose to make sure the variable is an actual value;
-			console.log(qId);
-      
-			//variable of the value of the LAST question id from the array of question ids.
-      if (qId.length == 0)
-      {
-        res.redirect('/home');
-      }
-      else
-      {
-			  var qId = qId[qId.length-1].question_id;
-        res.redirect("/question_forum/"+qId);
-      }
-		})
-		
-	});
+	//this is the user Id of the user askinng the question, that is taken out from the object newQ that was inserted in to the database
+	var userId= newQ.user_id;
+
+	db.query(sql_selectQuestion,[req.params.user_id], function(err,qId){
+		//debugging purpose to make sure the variable is an actual value;
+		console.log(qId);
+		//variable of the value of the LAST question id from the array of question ids.
+		if (qId.length == 0)
+		{
+			res.redirect('/home');
+		}
+		else
+		{
+			var qId = qId[qId.length-1].question_id;
+			res.redirect("/question_forum/"+qId);
+		}
+	})
+	
+});
 });
 
 module.exports = router;
--- FrontEnd/Website/routes/forum_page.js
+++ FrontEnd/Website/routes/forum_page.js
@@ -1,74 +1,70 @@
-var express = require('express')
-var router = express.Router();
+  var express = require("express");
+  var router = express.Router();
 
-var path = require('path');
-var util = require('util');
-var bodyParser = require("body-parser");
-var date = require('date-and-time');
+  var path = require("path");
+  var util = require("util");
+  var bodyParser = require("body-parser");
+  var date = require("date-and-time");
 
-// Database connection
-const mysql = require('mysql');
-var db = require('../database/database');
+  // Database connection
+  const mysql = require("mysql");
+  var db = require("../database/database");
 
-/** <<<<<<<<<<<<<<< Forum page >>>>>>>>>>>>>>
-* ============================================================================
-* Add comments here
-* ============================================================================
-*/
+  /** <<<<<<<<<<<<<<< Forum page >>>>>>>>>>>>>>
+  * ============================================================================
+  * Add comments here
+  * ============================================================================
+  */
 
-//Static qId to fake loads of the last question from the database, must update everytime the server is open
+  //* Listens for the question page request - done through the search */
+  router.get("/:q_id", (req, res) => {
+    var qId = req.params.q_id;
+    // Verify that the question id is a number
+    if (isNaN(req.params.q_id))
+    {
+      res.render("invalid_page", null);
+      return;
+    }
+  //query from question table joined with user's table user_id
+  var sql_getQuestion = "select question.question_title,question.question_body, question.datetime_asked, user.user_id, \
+  question.question_id, user.username AS asked_by FROM question JOIN user ON question.user_id = user.user_id \
+  WHERE question_id = ?";
 
-//* Listens for the question page request - done through the search */
-router.get('/:q_id', (req, res) => {
-	var qId = req.params.q_id;
-	// Verify that the question id is a number
-	if (isNaN(req.params.q_id))
-	{
-		res.render('invalid_page', null);
-		return;
-	}
-	//query from question table joined with user's table user_id
-	var sql = "select question.question_title,question.question_body, question.datetime_asked, user.user_id, \
-	question.question_id, user.username AS asked_by FROM question JOIN user ON question.user_id = user.user_id \
-	WHERE question_id = ?"
-	
-	db.query(sql,[qId], function(err,result){
-		if(err){
-			console.log(err);
-			return;
-		}
-		//debugging to show question object
-		console.log("FORUM RESULT " + util.inspect(result));
-		
-		//second query getting answer table and also joined with user's table user_id (yes, this is a query inside a query)
-		var sql2 ="select answer.answer_body, answer.answer_id, answer.datetime_answered, user.user_id, \
-		user.username AS answered_by FROM answer JOIN user ON answer.user_id = user.user_id \
-		WHERE answer.question_id = ?";
+  db.query(sql_getQuestion,[qId], function(err,result){
+    if(err){
+      console.log(err);
+      return;
+    }
+    //debugging to show question object
+    console.log("FORUM RESULT " + util.inspect(result));
+    
+    //second query getting answer table and also joined with user's table user_id (yes, this is a query inside a query)
+    var sql_getAnswers ="select answer.answer_body, answer.answer_id, answer.datetime_answered, user.user_id, \
+    user.username AS answered_by FROM answer JOIN user ON answer.user_id = user.user_id \
+    WHERE answer.question_id = ?";
 
+    var sql_totalAnswer = "SELECT answer.answer_id, SUM(CASE WHEN score= '1' THEN 1 ELSE 0 END) AS positiveScore, Sum(CASE WHEN score = '-1' THEN 1 ELSE 0 END) AS negativeScore FROM answer JOIN score_answer \
+    ON score_answer.answer_id = answer.answer_id GROUP BY score_answer.answer_id"
 
-
-		var sql_totalAnswer = "SELECT answer.answer_id, SUM(CASE WHEN score= '1' THEN 1 ELSE 0 END) AS positiveScore, Sum(CASE WHEN score = '-1' THEN 1 ELSE 0 END) AS negativeScore FROM answer JOIN score_answer \
-		ON score_answer.answer_id = answer.answer_id GROUP BY score_answer.answer_id"
-
-		db.query(sql2,[qId], function(err,answer){
+    db.query(sql_getAnswers,[qId], function(err,answer){
       if (err) res.redirect('/home');
       
       else
         db.query(sql_totalAnswer,function(err,total_ans){
 
-                    console.log("Total answers votes "+ util.inspect( total_ans));
+          //debugging to show total votes of answers
+          console.log("Total answers votes "+ util.inspect( total_ans));
 
                     //This function compares the answer and score_answer tables. If there is a row with same answer_if on both table
                     //It takes its sum of votes as positiveScore or negativeScore and also the answer id, and stores them in an array commonAnsId.
                     function intersect_arrays(answer, total_ans) {
                       var sorted_a = answer.concat().sort();
                       var sorted_b = total_ans.concat().sort();
-                      var commonAnsId =[]
+                      var commonAnsId =[];
                       var a_i = 0;
                       var b_i = 0;
 
-                      while (a_i < answer.length
-                        && b_i < total_ans.length)
+                      while (a_i < answer.length && b_i < total_ans.length)
                       {
                         if (sorted_a[a_i].answer_id === sorted_b[b_i].answer_id) {
                           commonAnsId.push(
@@ -95,7 +91,7 @@ router.get('/:q_id', (req, res) => {
                     var FirstInter = intersect_arrays(answer, total_ans);
                     console.log("Intersetction " + util.inspect(FirstInter));
 
-          //debugging
+          //debugging to show list of answers
           console.log("List of answers "+util.inspect(answer));
 
             //Object to be sent to forum page
@@ -106,24 +102,18 @@ router.get('/:q_id', (req, res) => {
               title : result[result.length-1].question_title,
               body : result[result.length-1].question_body,
               userID: result[result.length-1].user_id,
-
               // following code implemented using SQL2 query
               user_asked: result[result.length-1].asked_by,
               question_pts: null,
               datetime_asked:(new Date(result[result.length-1].datetime_asked)).toISOString().split("T")[0],
 
-
               // for answers, we store all the answers related to the given question_id in an array, where we iterate through the ANSWER object that was posted to the database from SQL2 query
               answers:      
               (function() {
                 arr = [];
-
-
                 /*The following code uses the function created before. From the intersection we can have 3 scenarios. When there are no 
                 common answer_id between both tables. When the number of answer_id in common between both tables is less than the num of answer for 
                 that question (answer.lenth) and when the number of common answer_id between both tables is the same as num of answers (answer.length)*/
-
-
 
                 //First Scenario
                 //Meaning no answer has a vote yet.
@@ -137,7 +127,6 @@ router.get('/:q_id', (req, res) => {
                   }
                 }
 
-
                 //Second Scenario 
                 //Meaning some answer have votes in it.
                 //I make a Second intersection that looks for common answer_id between the first intersection and the answer object taken from the query database (can explain more in person)
@@ -147,39 +136,36 @@ router.get('/:q_id', (req, res) => {
                   var totalScoreAnsArr = new Array(answer.length)
                   var SecondInter =intersect_arrays(answer, FirstInter )
                   console.log("Second intersection "+ util.inspect(SecondInter))
-                    for(var j=0;j<SecondInter.length;j++){
-                      for (var n =0; n<answer.length;n++){
-                        var totalScoreAns = 0;
+                  for(var j=0;j<SecondInter.length;j++){
+                    for (var n =0; n<answer.length;n++){
+                      var totalScoreAns = 0;
 
-
-                        if(SecondInter[j].answer_id==answer[n].answer_id){
-                          if (SecondInter[j].positiveScore != null){
-                            totalScoreAns += SecondInter[j].positiveScore;
-                          }
-
-
-                          if (SecondInter[j].negativeScore != null){
-                            totalScoreAns -= SecondInter[j].negativeScore;
-                          }
-
-
-                          totalScoreAnsArr[n]=totalScoreAns;
-                          console.log("Total answer = "+ totalScoreAns);	
-
+                      if(SecondInter[j].answer_id==answer[n].answer_id){
+                        if (SecondInter[j].positiveScore != null){
+                          totalScoreAns += SecondInter[j].positiveScore;
                         }
-                        if(totalScoreAnsArr[n]==null){
-                          totalScoreAnsArr[n]=0;
+
+                        if (SecondInter[j].negativeScore != null){
+                          totalScoreAns -= SecondInter[j].negativeScore;
                         }
+
+                        totalScoreAnsArr[n]=totalScoreAns;
+                        console.log("Total answer = "+ totalScoreAns);  
                       }
 
-                    }console.log(totalScoreAnsArr);
-                }
+                      if(totalScoreAnsArr[n]==null){
+                        totalScoreAnsArr[n]=0;
+                      }
+                    }
 
+                  }
+                  //Debugging to get the array fo total score of answers
+                  console.log(totalScoreAnsArr);
+                }
 
                 //Third scenario when length of first intersection is the same as number of answer
                 //Meaning all answer have some votes.
                 else if (FirstInter.length == answer.length){
-
 
                   var totalScoreAnsArr = new Array(answer.length)
                   for(var k = 0; k< answer.length; k++){
@@ -193,15 +179,15 @@ router.get('/:q_id', (req, res) => {
                         totalScoreAns -= FirstInter[k].negativeScore;
 
                       totalScoreAnsArr[k]=totalScoreAns;
-                      console.log("Total answer = "+ totalScoreAnsArr);									
-                    }								
-
+                      //Another debugging measure to get total answer arrays, making sure it is getting updated
+                      console.log("Total answer = "+ totalScoreAnsArr);                 
+                    }               
                   }
                 }
 
                 //Same as before, it iterates through num of answers and now answer_pts is set to values in
                 //totalScoreAnsArr array, which we also made sure its the same length as answer output.
-                for(var i = 0; i< answer.length; i++){								
+                for(var i = 0; i< answer.length; i++){                
                   arr.push(
                   {
                     answer_id: answer[i].answer_id,
@@ -215,12 +201,9 @@ router.get('/:q_id', (req, res) => {
 
               })()
             };
-
-
           // Get the question points
           //Query takes the sum of the total positive score, and total negative score for the question_id from the score_question table.
           var sql_totalScore = "SELECT SUM(CASE WHEN score= '1' THEN 1 ELSE 0 END) AS positiveScore, Sum(CASE WHEN score = '-1' THEN 1 ELSE 0 END) AS negativeScore FROM score_question WHERE question_id = ?";
-
           db.query(sql_totalScore, [qId], function(err, result_q1)
           {
             if (err)
@@ -237,40 +220,38 @@ router.get('/:q_id', (req, res) => {
 
             if (result_q1[0].negativeScore != null)
               totalScore -= result_q1[0].negativeScore;
-
             outputQ.question_pts = totalScore;
-
+            // Debugging measure to show if the outputQ object is being sent with the proper values
             console.log("outputQ "  + util.inspect(outputQ) + " " + (new Date(outputQ.datetime_asked)).toISOString().split("T")[0]);
             res.render('forum_page.ejs', {forum: outputQ});
-
           });
-        });	
+        }); 
 
-  });
+});
+});
+});
+
+  /*POST THE ANSWER ON THE ANSWER BOX, THERE IS A QUERY INSIDE. AND YES THIS APP.POST IS INSIDE THE APP.GET FROM ABOVE*/
+  router.post("/answer_to/:q_id/:user_answered", function(req, res) {
+
+  //OBJECTED TO BE POSTED TO ANSWER TABLE
+  var newA = {
+    answer_body : req.body.answer_body,
+    user_id : req.params.user_answered, // Can also be accessed by req.session.user_id
+    question_id : req.params.q_id,
+    datetime_answered :  date.format(new Date(), 'YYYY-MM-DD h:m:s'), 
+  }
+
+  //MYSQL QUERRY
+  var sql_insertAnswer =" insert into answer set ?";
+  db.query(sql_insertAnswer, newA, function(err,result){
+    if(err){
+      console.log(err);
+      return;
+    }
+    console.log("Answer succesfully added ")
+    res.redirect("/question_forum/" + req.params.q_id);
   });
 });
 
-/*POST THE ANSWER ON THE ANSWER BOX, THERE IS A QUERY INSIDE. AND YES THIS APP.POST IS INSIDE THE APP.GET FROM ABOVE*/
-router.post("/answer_to/:q_id/:user_answered", function(req, res) {
-	
-	//OBJECTED TO BE POSTED TO ANSWER TABLE
-	var newA = {
-		answer_body : req.body.answer_body,
-		user_id : req.params.user_answered, // Can also be accessed by req.session.user_id
-		question_id : req.params.q_id,
-		datetime_answered :  date.format(new Date(), 'YYYY-MM-DD h:m:s'), 
-	}
-	
-	//MYSQL QUERRY
-	var sql =" insert into answer set ?";
-	db.query(sql, newA, function(err,result){
-		if(err){
-			console.log(err);
-			return;
-		}
-		console.log("Answer succesfully added ")
-		res.redirect("/question_forum/" + req.params.q_id);
-	});
-});
-
-module.exports = router;
+  module.exports = router;
