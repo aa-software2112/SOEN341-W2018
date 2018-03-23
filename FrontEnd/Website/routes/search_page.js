fc008c6c5f52898870eccddcfe1b52cee7be1ecc
Abishand <35385222+AA-97@users.noreply.github.com>
2018-03-22 20:04:03 -0400

    Refactor search page
    Code refactoring enforcing the coding style format for search page

--- FrontEnd/Website/routes/search_page.js
+++ FrontEnd/Website/routes/search_page.js
@@ -1,15 +1,15 @@
-var express = require('express')
+var express = require("express")
 var router = express.Router();
 
-var path = require('path');
-var util = require('util');
+var path = require("path");
+var util = require("util");
 var bodyParser = require("body-parser");
-var date = require('date-and-time');
-var dateFormat = require('dateformat');
+var date = require("date-and-time");
+var dateFormat = require("dateformat");
 
 // Database connection
-const mysql = require('mysql');
-var db = require('../database/database');
+const mysql = require("mysql");
+var db = require("../database/database");
 
 /** <<<<<<<<<<<<<<< Search page >>>>>>>>>>>>>>
 * ============================================================================
@@ -22,17 +22,18 @@ var db = require('../database/database');
 // Listens for a request to the search page and renders it with the results
 // GET command should not be used - user should only get to search page via a search POST
 // Gets data from the search bar and renders the search page with the results
-router.get('/search_on', (req,res) => {
+router.get("/search_on", (req,res) => {
 	var keyword = req.query.search;
 	
 	/**
 	*  Query to match the keyword to the question title of the question's table in the database. Also return the
 	question id for redirecting the user to the forum page upon clicking on a matched question. 
 	*/
-	var search_keyword = 'SELECT question.question_id, question.question_title, question.user_id, user.user_id, user.username, question.datetime_asked FROM question, user WHERE Match(question_title) Against("'+ keyword +'" IN NATURAL LANGUAGE MODE) AND question.user_id = user.user_id';
-	var output = ' ';
+	//causes error when using double quotation on query
+	var searchKeyword = 'SELECT question.question_id, question.question_title, question.user_id, user.user_id, user.username, question.datetime_asked FROM question, user WHERE Match(question_title) Against("'+ keyword +'" IN NATURAL LANGUAGE MODE) AND question.user_id = user.user_id';
+	var output = " ";
 	
-	db.query(search_keyword, function(err, rows, fields) {
+	db.query(searchKeyword, function(err, rows, fields) {
 		if (err){
 			console.log(err);
 		}
@@ -45,19 +46,19 @@ router.get('/search_on', (req,res) => {
 					msg: "No result found :) "
 				},
 				
-				search_question: {
-					search_question_list:
+				searchQuestion: {
+					searchQuestionList:
 					(function() {
 						var data=[];
-						var number_of_questions = rows.length;
-						for (var i = 0; i < number_of_questions; i++) {
+						var numberOfQuestions = rows.length;
+						for (var i = 0; i < numberOfQuestions; i++) {
 							
 							var searchList = {
-								questionID: ' ',
-								questionList: ' ',
-								userID: ' ',
-								userName: ' ',
-								date_ask: ' '
+								questionID: " ",
+								questionList: " ",
+								userID: " ",
+								userName: " ",
+								date_ask: " "
 								
 							}						
 							data.push(searchList);
@@ -68,30 +69,30 @@ router.get('/search_on', (req,res) => {
 				}
 			}
 		} else {
-			// Stores the result into the array data, which is returned when the function search_question_list is called
+			// Stores the result into the array data, which is returned when the function searchQuestionList is called
 			output = {
 				
 				message:  {
 					msg: "Results for: " + keyword
 				},
 				
-				search_question: {
-					search_question_list:
+				searchQuestion: {
+					searchQuestionList:
 					(function() {
 						var data=[];
-						var number_of_questions = rows.length;
-						for (var i = 0; i < number_of_questions; i++) {
+						var numberOfQuestions = rows.length;
+						for (var i = 0; i < numberOfQuestions; i++) {
 							
 							var timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
-							var d = (new Date(rows[i].datetime_asked  - timezoneOffset)).toISOString().split("T");
-							d = d[0] + " - " + d[1].split("Z")[0].slice(0, -4);
+							var date = (new Date(rows[i].datetime_asked  - timezoneOffset)).toISOString().split("T");
+							date = date[0] + " - " + date[1].split("Z")[0].slice(0, -4);
 							
 							var searchList = {
 								questionID: rows[i].question_id,
 								questionList: rows[i].question_title,
 								userID: rows[i].user_id,
 								userName: rows[i].username,
-								date_ask: d						
+								date_ask: date						
 							}						
 							data.push(searchList);
 							
@@ -103,7 +104,7 @@ router.get('/search_on', (req,res) => {
 			}
 		}
 		
-		res.render('search_page.ejs', {search_page: output});
+		res.render("search_page.ejs", {search_page: output});
 	})
 });
 
