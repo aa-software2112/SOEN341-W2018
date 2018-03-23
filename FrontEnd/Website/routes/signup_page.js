a610d594ddae8b0558c1f155507afcb4b3d1295e
aa-software2112 <a_dreoli@encs.concordia.ca>
2018-03-22 20:07:40 -0400

    Refactored code based on sprint4 code style
    The code in the about. contact, and signup page was refactored according
    to the team's self-made coding style decisions

--- FrontEnd/Website/routes/signup_page.js
+++ FrontEnd/Website/routes/signup_page.js
@@ -1,14 +1,14 @@
-var express = require('express')
+var express = require("express")
 var router = express.Router();
 
-var path = require('path');
-var util = require('util');
+var path = require("path");
+var util = require("util");
 var bodyParser = require("body-parser");
-var date = require('date-and-time');
+var date = require("date-and-time");
 
 // Database connection
-const mysql = require('mysql');
-var db = require('../database/database');
+const mysql = require("mysql");
+var db = require("../database/database");
 
 /** <<<<<<<<<<<<<<< Signup page >>>>>>>>>>>>>>
 * ============================================================================
@@ -19,54 +19,52 @@ var db = require('../database/database');
 */
 
 //listens for signup page request to load it
-router.get('/', (req,res) => {
-	res.render('sign_up.ejs');
-});
-
-router.get('/confirmation', (req, res) => {
+router.get("/", function(req,res) {
 	
-	res.render('after_signup');
+	res.render("sign_up.ejs");
 	
 });
 
-router.get('/confirmation/:uname/:email', (req, res) => {
+router.get("/confirmation", function(req, res) {
+	
+	res.render("after_signup");
+	
+});
+
+router.get("/confirmation/:uname/:email", function(req, res) {
 	
 	console.log(req.params);
-	res.render('after_signup', {username: req.params.uname, email: req.params.email});
+	res.render("after_signup", {username: req.params.uname, email: req.params.email});
 	
 });
 
 /* logs info from user from signup page*/
-router.post('/', (req,res) => {
+router.post("/", function(req,res) {
 	
 	// Query check if user already exists
-	var check_exists = "SELECT " + 
-	"SUM(CASE WHEN email = ? then 1 else 0 end) email_count, " + 
-	"SUM(CASE WHEN username = ? then 1 else 0 end) user_count " +
-	"FROM user;"; 
+	var checkIfUserExists = "SELECT SUM(CASE WHEN email = ? then 1 else 0 end) \
+	email_count, SUM(CASE WHEN username = ? \
+	then 1 else 0 end) user_count FROM user"; 
 	
-	db.query(check_exists, [req.body.email, req.body.uName], function(err, result)
-	{
+	db.query(checkIfUserExists, [req.body.email, req.body.uName], function(err, result) {
 		if (err)
 		{
 			console.log("Email check query failed " + err);
 			return;
 		}
+		
 		result = result[0]; // Get passed the array
 		
 		// The email already exists, signup failed
-		if (result.email_count > 0)
-		{
+		if (result.email_count > 0) {
 			res.render("sign_up", {msg: "The email " + req.body.email + " already exists."});
 		}
 		// The username already exists, signup failed
-		else if (result.user_count > 0)
-		{
+		else if (result.user_count > 0) {
 			res.render("sign_up", {msg: "The username " + req.body.uName + " already exists."});	
 		}
 		// The email and username don't exist, signup succeeded
-		else
-		{
+		else {
 			
 			//THIS IS THE OBJECT TO BE POSTED IN THE DATABSE USER TABLE. EVERY DATA IS GOTTEN FROM THE SIGN UP FORM
 			var newUser = {
@@ -77,32 +75,24 @@ router.post('/', (req,res) => {
 				email : req.body.email,
 				password : req.body.password,
 				country : req.body.country,
-				birth_date : req.body.year +"-" + req.body.month + "-"+req.body.day,
+				birthDate : req.body.year +"-" + req.body.month + "-"+req.body.day,
 				gender : req.body.gender,
-				datetime_entered : date.format(new Date(), 'YYYY-MM-DD h:m:s'), 
+				datetimeEntered : date.format(new Date(), "YYYY-MM-DD h:m:s"), 
 				
 			};
 			
 			// Add user to database
 			db.query("insert into user set ? ", newUser, function(err,result){
-				if(err){
+				if(err) {
 					console.log(err);
 					res.render("sign_up", {msg: "Database Error on Signup"});
-				}
-				else
-				{
+				} else {
 					console.log("User succesfully added!")
 					res.redirect("/sign_up/confirmation/" + newUser.username + "/" + newUser.email);
 				}
 			});
-		}
-		
-		
+		}	
 	});
-	
-	
-	
-	
 });
 
 
