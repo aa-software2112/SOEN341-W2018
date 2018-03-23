9db624830847402a8c7295d601da0ae5fae86692
TSer1111 <serranotom@gmail.com>
2018-03-23 11:03:56 -0400

    Modified the sign up test file to account for a failed sign up attempt. Runs alongside the successful sign up test

--- FrontEnd/Website/test/signup.test.js
+++ FrontEnd/Website/test/signup.test.js
@@ -9,13 +9,20 @@ describe("SIGN UP", function () {
 			.expect(200, done);
 	});
 
-		// Checks to ensure that form data is being sent over the server
+		// Checks to ensure that form data is being sent over the server and sign up passes
 	it("Sign up page sends form data across server and to the DB", function(done) {
 		request(app).post("/sign_up")
 			.send({fName: "First", lName: "last", uName: "Tester", email: "testing@test.ca", password: "Test12", country: "Canada", 
 			year: "1905", month: "10", day: "12", gender: "M" })
+			.expect(302, done);
+	});
+
+	// Checks to ensure that the form data is being sent over the server and sign up fails
+	it("Sign up page sends form data across server and to the DB", function(done) {
+		request(app).post("/sign_up")
+			.send({fName: "First", lName: "last", uName: "Tester", email: "test@test.ca", password: "Test12", country: "Canada", 
+			year: "1905", month: "10", day: "12", gender: "M" })
 			.expect(200, done);
 	});
-	
 
 });
