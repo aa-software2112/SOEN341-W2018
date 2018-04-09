0c626ae63c780954fcbb170c56f3a16638606412
unknown <serranotom@gmail.com>
2018-04-08 22:48:10 -0400

    Added a test that checks that the the login page remains functional when the user tries to login with an empty form

--- FrontEnd/Website/test/login.test.js
+++ FrontEnd/Website/test/login.test.js
@@ -16,10 +16,17 @@ describe("LOGIN PAGE", function () {
 		.expect(302, done);
 	});
 
-	// Tests if the login page form posts its data on the server and login does not work (302 response)
-	it("Invalid User - Login page sends form data across server and to the cookie", function(done) {
+	// Tests that the page remains functional after a failed login attempt
+	it("Invalid User - Login page remains functional after the user tries to login with as a non-existent user", function(done) {
 		request(app).post("/login")
 		.send({email: "doesnotexist@test.ca", password: "Test12"})
+		.expect(200, done);
+	});
+
+	// Tests if the page remains functional if the user tries to login with no form data
+	it("Invalid Data (empty form) - Login page remains functional after the user tries to login with an empty form", function(done) {
+		request(app).post("/login")
+		.send({email: "", password: ""})
 		.expect(200, done);
 	});
 });
